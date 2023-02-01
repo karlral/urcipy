140f369a96e955a3849838b614d398901a6129bf
karlral <karlral@gmail.com>
2023-01-31 23:02:51 -0300

    Es el proyecto de cero añadido

---
+++ src/main/java/com/sistema/urcipy/UrcipyApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.urcipy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrcipyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrcipyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sistema/urcipy/UrcipyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.urcipy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrcipyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
